dba349a5a0d42b840b9d376a00f300cc98a85d5d
Vansh Patel <vansh.p2805@gmail.com>
2026-02-16 11:21:45 +0530

    Feb16 we did access specifiers

---
+++ Feb16.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Feb16 {
+    public static void main(String args[]){
+        int[] numbers1 = {10,20,30,40,50,6,7,8,9,0};
+        int[] numbers2 = {60,70,80,90,100};
+        int[] numbers3 = {1,2,3,4,5};
+        int[] numbers4 = new int[10];
+
+        for (int i=0; i<numbers4.length+1; i++){
+            numbers4[i] = numbers1[i]; //exception ArrayIndexOutOfBounds 
+        }
+
+        for (int i=0; i<numbers1.length; i++){
+            System.out.print(numbers1[i]+",");
+        }
+
+        System.out.println("");
+        
+        for (int num1 : numbers1){
+            System.out.print(num1+",");
+        }
+        System.out.println("");
+        for (int num2 : numbers2){
+            System.out.print(num2+",");
+        }
+        System.out.println("");
+        for (int num3 : numbers3){
+            System.out.print(num3+",");
+        }
+
+
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+
+        System.out.println("Fruits: "+fruits);
+        fruits.remove("Banana");
+        System.out.println("after removing banana :- "+fruits);
+
+    }
+}
